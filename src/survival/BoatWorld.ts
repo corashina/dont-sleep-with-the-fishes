9acd8d063c9e8f7e8019f51423e69d43d8aa33cc
corashina <23419135+corashina@users.noreply.github.com>
2026-07-19 12:05:47 +0200

    feat: match survival buoyancy to scavenging

--- src/survival/BoatWorld.ts
+++ src/survival/BoatWorld.ts
@@ -36,7 +36,6 @@ import {
   sampleWaveFieldInto,
   type WaveSample,
 } from '../ocean/WaveField';
-import { applySurvivalBuoyancyComfortInto } from './survivalBuoyancyComfort';
 import { boatStorageTransform } from '../world/BoatStorage';
 import { createLifeboat, type LifeboatBuild } from '../world/Lifeboat';
 import { createProp } from '../world/PropFactory';
@@ -234,7 +233,6 @@ export class BoatWorld {
     sampleDefaultWaveInto,
   );
   private readonly boatPose: BoatPose = { ...INITIAL_BOAT_POSE };
-  private readonly rawBoatTargetPose: BoatPose = { ...INITIAL_BOAT_POSE };
   private readonly boatTargetPose: BoatPose = { ...INITIAL_BOAT_POSE };
   private previousBoatPitch = 0;
   private previousBoatRoll = 0;
@@ -473,15 +471,11 @@ export class BoatWorld {
 
     const amplitudeScale = weatherAmplitudeScale(this.weather);
     this.buoyancy.sampleTargetInto(
-      this.rawBoatTargetPose,
+      this.boatTargetPose,
       time,
       SURVIVAL_BOAT_ANCHOR.x,
       SURVIVAL_BOAT_ANCHOR.z,
       amplitudeScale,
-    );
-    applySurvivalBuoyancyComfortInto(
-      this.boatTargetPose,
-      this.rawBoatTargetPose,
     );
     smoothBoatPoseInto(this.boatPose, this.boatPose, this.boatTargetPose, delta, 7);
     this.applyBasePresentation();
